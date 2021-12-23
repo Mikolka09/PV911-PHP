3b0ac87175f16a78e2c1719e5e732d97c20ea36b
Микола Матіос <mikolka09@gmail.com>
2021-12-23 19:57:30 +0100

    23.12.2021

---
+++ lesson5/example-app/database/factories/CommentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'comment'=>$this->faker->sentence(),
+            'user_id'=>User::all()->random()->id,
+            'rate'=>$this->faker->numberBetween(1,5)
+        ];
+    }
+}
